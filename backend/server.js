d9051d7dcb40021e5b38da97a7f452519f4e7425
Shashi Ranjan <your_email@gmail.com>
2026-02-13 12:27:40 +0530

    add read route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
   res.send("Server is running 🚀");
 });
 
+app.get("/read", (req, res) => {
+  res.send("Read route working ✅");
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
